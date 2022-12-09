9b3fdc57b7feb64a57f7bb940fe5d89f4983096e
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-09 16:30:31 +0200

    Some fetch methods

--- dart/lab3/application/bin/application.dart
+++ dart/lab3/application/bin/application.dart
@@ -1,22 +1,39 @@
-import 'package:application/application.dart' as application;
+import 'dart:convert';
+
+import 'package:application/api.dart' as api;
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
-  var request =
-      http.Request('POST', Uri.parse('https://kpi.eu.auth0.com/oauth/token'));
-  request.bodyFields = {
-    'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
-    'scope': 'offline_access',
-    'username': 'proksima.maxim1@gmail.com',
-    'password': 'TheStrongPassword123',
-    'realm': 'Username-Password-Authentication'
-  };
+  // var request =
+  //     http.Request('POST', Uri.parse('https://kpi.eu.auth0.com/oauth/token'));
+  // request.bodyFields = {
+  //   'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
+  //   'scope': 'offline_access',
+  //   'username': 'proksima.maxim1@gmail.com',
+  //   'password': 'TheStrongPassword123',
+  //   'realm': 'Username-Password-Authentication',
+  //   'client_id': 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
+  //   'client_secret':
+  //       'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB'
+  // };
 
-  http.StreamedResponse response = await request.send();
+  // var response =
+  //     await http.post(Uri.parse('https://kpi.eu.auth0.com/oauth/token'), body: {
+  //   'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
+  //   'scope': 'offline_access',
+  //   'username': 'proksima.maxim1@gmail.com',
+  //   'password': 'TheStrongPassword123',
+  //   'realm': 'Username-Password-Authentication',
+  //   'client_id': 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
+  //   'client_secret':
+  //       'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB'
+  // });
+  var data = await api.getRefreshToken('https://kpi.eu.auth0.com/oauth/token');
+  print(data['refresh_token']);
 
-  if (response.statusCode == 200) {
-    print(await response.stream.bytesToString());
-  } else {
-    print(response.reasonPhrase);
-  }
+  // if (response.statusCode == 200) {
+  //   print(await response.stream.bytesToString());
+  // } else {
+  //   print(response.reasonPhrase);
+  // }
 }
--- dart/lab3/application/test/application_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:application/application.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
