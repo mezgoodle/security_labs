457e5714afaf1a4aa79a9601f76b9e3f17772392
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-09 17:14:08 +0200

    Set default parameters

--- dart/lab3/application/bin/application.dart
+++ dart/lab3/application/bin/application.dart
@@ -1,8 +1,10 @@
 import 'package:application/api.dart' as api;
 
 void main(List<String> arguments) async {
-  Map refreshTokenData =
-      await api.getRefreshToken('https://kpi.eu.auth0.com/oauth/token');
+  Map refreshTokenData = await api.getRefreshToken(
+      'https://kpi.eu.auth0.com/oauth/token',
+      'proksima.maxim1@gmail.com',
+      'TheStrongPassword123');
   String refreshToken = refreshTokenData['refresh_token'];
   String firstToken = refreshTokenData['access_token'];
   print('Refresh token is: $refreshToken');
--- dart/lab3/application/lib/api.dart
+++ dart/lab3/application/lib/api.dart
@@ -1,17 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<Map> getRefreshToken(String url) async {
+Future<Map> getRefreshToken(String url, String username, String password,
+    {String clientId = 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
+    String clientSecret =
+        'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB'}) async {
   var response = await http.post(Uri.parse(url), body: {
     'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
     'scope': 'offline_access',
-    'username': 'proksima.maxim1@gmail.com',
-    'password': 'TheStrongPassword123',
+    'username': username,
+    'password': password,
     'realm': 'Username-Password-Authentication',
-    'client_id': 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
-    'client_secret':
-        'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB'
+    'client_id': clientId,
+    'client_secret': clientSecret
   });
+
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
   }
@@ -19,12 +22,14 @@ Future<Map> getRefreshToken(String url) async {
       'Error: ${response.reasonPhrase}. Status: ${response.statusCode}');
 }
 
-Future<Map> refreshToken(String url, String token) async {
+Future<Map> refreshToken(String url, String token,
+    {String clientId = 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
+    String clientSecret =
+        'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB'}) async {
   var response = await http.post(Uri.parse(url), body: {
     'grant_type': 'refresh_token',
-    'client_id': 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
-    'client_secret':
-        'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB',
+    'client_id': clientId,
+    'client_secret': clientSecret,
     'refresh_token': token
   });
   if (response.statusCode == 200) {
