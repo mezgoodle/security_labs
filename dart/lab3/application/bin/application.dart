88460896262998705bd7fad601443f075a440bae
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-10 10:20:18 +0200

    Fetch data from the my application

--- dart/lab3/application/bin/application.dart
+++ dart/lab3/application/bin/application.dart
@@ -8,8 +8,28 @@ void main(List<String> arguments) async {
   String refreshToken = refreshTokenData['refresh_token'];
   String firstToken = refreshTokenData['access_token'];
   print('Refresh token is: $refreshToken');
-  Map tokenData = await api.refreshToken(
-      'https://kpi.eu.auth0.com/oauth/token', refreshToken);
+  Map tokenData = await api.refreshToken(config['KPI']['URL'], refreshToken);
   String secondToken = tokenData['access_token'];
+  print('Are the access tokens are similiar - ${firstToken == secondToken}');
+
+  print('\n');
+  print('Getting refresh token from the MY application');
+  refreshTokenData = await api.getRefreshToken(
+      config['MYSELF']['URL'],
+      config['MYSELF']['USERNAME'],
+      config['MYSELF']['PASSWORD'],
+      config['MYSELF']['CLIENT_ID'],
+      config['MYSELF']['CLIENT_SECRET'],
+      config['MYSELF']['AUDIENCE']);
+  refreshToken = refreshTokenData['refresh_token'];
+  firstToken = refreshTokenData['access_token'];
+  print('Refresh token is: $refreshToken');
+  tokenData = await api.refreshToken(
+    config['MYSELF']['URL'],
+    refreshToken,
+    config['MYSELF']['CLIENT_ID'],
+    config['MYSELF']['CLIENT_SECRET'],
+  );
+  secondToken = tokenData['access_token'];
   print('Are the access tokens are similiar - ${firstToken == secondToken}');
 }
--- dart/lab3/application/lib/config.dart
+++ dart/lab3/application/lib/config.dart
@@ -9,8 +9,12 @@ const Map config = {
     'AUDIENCE': 'https://kpi.eu.auth0.com/api/v2/'
   },
   'MYSELF': {
-    'URL': 'https://kpi.eu.auth0.com/oauth/token',
-    'USERNAME': 'proksima.maxim1@gmail.com',
-    'PASSWORD': 'TheStrongPassword123'
+    'URL': 'https://dev-b34fyn1cot22je3i.us.auth0.com/oauth/token',
+    'USERNAME': 'mezgoodle@gmail.com',
+    'PASSWORD': '1Max2Victor',
+    'CLIENT_ID': 'C4dFTmHwKV8DXaXkBoCX4RLAoENBsstZ',
+    'CLIENT_SECRET':
+        'Hiuy2N4AdwU-zezmgVKhKHhf-5TINv_82RvwpESbV25ddoUZZ88pRWBXmQVCJ7GB',
+    'AUDIENCE': 'https://dev-b34fyn1cot22je3i.us.auth0.com/api/v2/'
   }
 };
