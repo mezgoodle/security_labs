f8af7049da2459c528e9467cf55946e3ab7191cc
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-22 20:34:12 +0200

    Some changes

--- python/lab4/app/main/views.py
+++ python/lab4/app/main/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 
-from .utils import authenticate
+from .utils import authenticate, getUserId, getUserInfo
 
 
 def index(request):
+    user = None
+    error = False
     if request.method == "POST":
         data = request.POST
-        status, data = authenticate(data.get("login"), data.get("password"))
+        email = data.get("login")
+        status, data = authenticate(email, data.get("password"))
         if status == 200:
             request.session["access_token"] = data["access_token"]
             request.session["refresh_token"] = data["refresh_token"]
-    if token := request.session.get("access_token"):
-        print(token)
-    print(request.session)
-    return render(request, "login.html")
+            user_id = getUserId(email)
+            request.session["user_id"] = user_id
+        else:
+            error = True
+    if access_token := request.session.get("access_token"):
+        user_id = request.session["user_id"]
+        user = getUserInfo(access_token, user_id)
+        return render(request, "index.html", {"user": user[1]["name"]})
+    return render(request, "login.html", {"error": error})
